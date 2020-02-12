94daab7503a7a438fc70fc7dbfe86f698946beb3
Carlos Silva <carlosaraujo34@gmail.com>
2020-02-12 12:43:17 +0000

    house prices

--- hp.R
+++ hp.R
@@ -7,28 +7,53 @@ setwd("C:/Users/Utilizador/Desktop/kaggle/House_Prices-Advanced_Regression_Techn
 set.seed(4561)
 
 # Load data
-train <- read.csv('train.csv', stringsAsFactors = F)
-test <- read.csv('test.csv', stringsAsFactors = F)
+train <- read.csv('train.csv')
+test <- read.csv('test.csv')
 glimpse(train)
 glimpse(test)
 test$SalePrice <- NA
 hp <- rbind(train, test)
 remove(train); remove(test)
 glimpse(hp)
-hp %>% skim()
+skim(hp)
 
-## Fix variable name starts with number
+# Changing MSSubClass to a factor
+
+hp <- hp %>%
+        mutate(MSSubClass = factor(MSSubClass))
+
+# Fix variable name starts with number
 
 names(hp)[44:45] <- c("FirstFLSF","SecFLSF")
 names(hp)[70] <- c("ThreeSnPorch")
 
-# Missing data
+# Fix typos in factorial levels
 
-apply(sapply(hp, is.na),2,sum)
-DataExplorer::plot_missing(hp)
+library(forcats)
+dplyr::count(hp, RoofMatl)
+hp <- hp %>%
+        mutate(RoofMatl = fct_collapse(RoofMatl, "TarGrv" = "Tar&Grv"))
+dplyr::count(hp, RoofMatl)
 
-# Feature engineering
+dplyr::count(hp, Exterior1st)
+hp <- hp %>%
+        mutate(Exterior1st = fct_collapse(Exterior1st, "WdSdng" = "Wd Sdng"))
 
-## MSSubClass
+dplyr::count(hp, Exterior2nd)
+hp <- hp %>%
+        mutate(Exterior2nd = fct_collapse(Exterior2nd, "BrkComm" = "Brk Cmn", "WdSdng" = "Wd Sdng", "WdShng" = "Wd Shng"))
+
+table(hp$GarageYrBlt)
+
+# Fix typo of GarageYrBlt
+
+summary(hp$GarageYrBlt)
+hp <- hp %>% 
+        mutate(GarageYrBlt = ifelse(GarageYrBlt > 2010, NA, GarageYrBlt))
+
+
+# remove Outliers
+
+
 
 
